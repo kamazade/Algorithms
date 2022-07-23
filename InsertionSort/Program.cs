9714875e013af619d33c386933d09345c31f1c74
kamazade <kamazadealper@gmail.com>
2022-07-23 14:42:20 +0300

    Initial commit

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace InsertionSort
+{
+    class Program
+    {
+        public static int[] Insertion(int[] arr){
+            
+            for(int i = 1; i<arr.Length;i++){
+                int step = i;
+                while(step>0){
+                    if(arr[step-1] > arr[step]){
+                        int pre = arr[step-1];
+                        arr[step-1] = arr[step] ;
+                        arr[step] = pre;
+                    }else{
+                        break;
+                    }
+                    step--;
+                }
+            }
+
+
+            return arr;
+        }
+
+        
+        static void Main(string[] args)
+        {
+            int[] insertion = Insertion(new int[]{22,27,16,2,18,6});
+        
+            foreach (var item in insertion)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
